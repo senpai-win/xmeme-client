d4a296348a5c444f6bd0dc337d7eeb24cdf36c06
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-02-15 02:10:48 +0530

    added posted at

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,15 +2,17 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import edit from "../assets/images/edit.svg";
 import trash from "../assets/images/trash.svg";
+import ReactTimeAgo from 'react-time-ago'
 
 const Meme = ({
-  details: { id, name, url, caption },
+  details: { id, name, url, caption, createdAt },
   selectMeme,
   deleteMeme,
 }) => (
   <div>
-    <Card className="card mx-2.5 my-2.5" style={{ width: "28rem" }}>
+    <Card className="card mx-2.5 my-2.5 py-0.5" style={{ width: "28rem" }}>
       <Card.Body>
+      <div className = "flex flex-col">
         <div className="flex flex-row items-baseline justify-between">
           <Card.Title>{name}</Card.Title>
           <div className="flex flex-row">
@@ -34,6 +36,10 @@ const Meme = ({
             />
           </div>
         </div>
+        <span className = 'text-gray-700 text-sm'>
+         Posted <ReactTimeAgo date={createdAt} locale="en-US"/>
+        </span>
+      </div>
         <Card.Text className="font-sans">{caption}</Card.Text>
       </Card.Body>
       <Card.Img variant="top" src={url} />
